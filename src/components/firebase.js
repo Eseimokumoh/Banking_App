231740e21fa5fec360e9f654b4d8e16478a747cc
Eseimokumoh <119541134+Eseimokumoh@users.noreply.github.com>
2023-08-31 09:48:58 +0800

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD33_WnOOyDdjchHk_9lxP5IlescFtRIP8",
-  authDomain: "bnp-paribas-dc025.firebaseapp.com",
-  projectId: "bnp-paribas-dc025",
-  storageBucket: "bnp-paribas-dc025.appspot.com",
-  messagingSenderId: "115308313678",
-  appId: "1:115308313678:web:1cf5d737dc9bda631b971f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
